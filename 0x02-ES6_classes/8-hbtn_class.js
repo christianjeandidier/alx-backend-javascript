40e11ce6bb9e42bb9a72068243bf8679ab503449
christianjeandidier <ndo.jeandidier@gmail.com>
2024-10-03 18:03:59 +0000

    8. Primitive - Holberton Class

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,7 +1,32 @@
-export default function divideFunction(numerator, denominator) {
-  if (denominator === 0) {
-    throw Error('cannot divide by 0');
-  } else {
-    return numerator / denominator;
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this.size;
+    }
+    if (hint === 'string') {
+      return this.location;
+    }
+    return this;
   }
 }
